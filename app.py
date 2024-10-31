3bbfb3efa1b8fcac684e85e802c0b787e2ab0284
F1 <fmmirza@iuj.ac.jp>
2024-10-31 13:49:14 +0900

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import datetime
 import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
 
 import appdirs as ad
 ad.user_cache_dir = lambda *args: "/tmp"
@@ -33,9 +35,34 @@ if stock is not None:
 else:
   st.error("Failed to fetch historical data.")
 
-data = yf.download(symbol,start=sdate,end=edate)
+from datetime import datetime
+
+start_date = st.slider("Data from:", value = datetime(2020, 1, 1), format = "DD/MM/YYYY")
+st.write("Start Date:", start_date)
+
+end_date = st.slider("To:", value = datetime(2023, 1, 1), format = "DD/MM/YYYY")
+st.write("End Date:", end_date)
+
+data = yf.download(symbol,start=start_date,end=end_date)
+
 if data is not None:
+
   st.write(data.describe())
-  st.line_chart(data['Close'],x_label="Date",y_label="Close")
+  chart_data = pd.DataFrame((data['Close']))
+  st.area_chart(chart_data)
+
 else:
-    st.error("Failed to fetch historical data.")
+  st.error("Failed to fetch historical data.")
+
+@st.cache_data
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode("utf-8")
+
+csv = convert_df(data)
+
+st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name="Data.csv",
+    mime="text/csv")
